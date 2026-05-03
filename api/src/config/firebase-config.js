eccbdbd525c21214f1bbcfb3db6dd022a47290da
Edwards Ardila <edwards.ardila03@gmail.com>
2026-05-02 20:38:30 -0500

    Fix Firebase init: make it conditional when env var is not set

--- api/src/config/firebase-config.js
+++ api/src/config/firebase-config.js
@@ -1,11 +1,12 @@
 require("dotenv").config();
-const {FIREBASE_SERVICE_ACCOUNT_KEY} = process.env;
+const { FIREBASE_SERVICE_ACCOUNT_KEY } = process.env;
 const admin = require("firebase-admin");
 
-const serviceAccount = JSON.parse(FIREBASE_SERVICE_ACCOUNT_KEY || {});
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
+if (FIREBASE_SERVICE_ACCOUNT_KEY) {
+  const serviceAccount = JSON.parse(FIREBASE_SERVICE_ACCOUNT_KEY);
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
 
 module.exports = admin;
